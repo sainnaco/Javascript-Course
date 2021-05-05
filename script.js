5e1a08e61e4f5a7ee44393e20406c3c678faa407
Amin Pourzare <apourzare@gmail.com>
2021-05-05 16:47:54 +0430

    Session 001

---
+++ script.js
@@ -0,0 +1,73 @@
+// console.log("Hello from console");
+// alert("Hello from alert");
+// document.write("Hello from document");
+
+// var age = 31;
+// var name = "Amin";
+// var isMale = true;
+// let age = 31;
+// let name = "Amin";
+// let isMale = true;
+// // console.log(typeof name);
+// // console.log(typeof age);
+// // console.log(typeof isMale);
+// console.log(name);
+// console.log(age);
+// console.log(isMale);
+
+
+
+// var vs let
+// var fname = "Amin";
+// let fname = "Amin";
+// console.log(fname);
+// // 
+// // 
+// // 
+// // var fname = "Mohadese";
+// // let fname = "Mohadese";
+// fname = "Mohadese";
+// console.log(fname);
+
+// var fname1 = "Amin";
+// let fname2 = "Mehrsa";
+
+// {
+//     var fname1 = "Jafar";
+//     let fname2 = "Zahra";
+// }
+
+// console.log(fname1);
+// console.log(fname2);
+
+
+
+// console.log(fname);
+// // var fname = "Amin";
+// let fname = "Amin";
+
+// 2 * 2 = 4
+
+// const fname = "Amin";
+// console.log(fname);
+// fname = "Zahra";
+// console.log(fname);
+
+// Primitive Data Type
+
+// number
+// string 
+// boolean 
+// null 
+// undefined 
+// symbol 
+
+
+
+// non-Primitive Data Type
+
+// array
+// object
+// function
+// date
+// regix
