4735c44aba55e3c3bc701702c51c553d99bd96dc
Amin Pourzare <apourzare@gmail.com>
2021-05-14 16:39:49 +0430

    Session 02 -> Work with Primitive Data

--- script.js
+++ script.js
@@ -1,73 +1,66 @@
-// console.log("Hello from console");
-// alert("Hello from alert");
-// document.write("Hello from document");
-
-// var age = 31;
-// var name = "Amin";
-// var isMale = true;
-// let age = 31;
-// let name = "Amin";
-// let isMale = true;
-// // console.log(typeof name);
-// // console.log(typeof age);
-// // console.log(typeof isMale);
-// console.log(name);
-// console.log(age);
-// console.log(isMale);
-
-
-
-// var vs let
-// var fname = "Amin";
 // let fname = "Amin";
-// console.log(fname);
-// // 
-// // 
-// // 
-// // var fname = "Mohadese";
-// // let fname = "Mohadese";
-// fname = "Mohadese";
-// console.log(fname);
+// let lname = "Pourzare";
+// console.log(fname + " " + lname);
+// console.log(`${fname} ${lname}`);
 
-// var fname1 = "Amin";
-// let fname2 = "Mehrsa";
+let text = "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد."
+// console.log(text.length);
+// console.log(text[0]);
+// console.log(text.slice(0,4));
+// console.log(text.substr(5,40));
+// console.log(text.includes("لورم"));
 
-// {
-//     var fname1 = "Jafar";
-//     let fname2 = "Zahra";
-// }
+// let en_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet."
+// console.log(en_text.toLocaleLowerCase());
+// console.log(en_text.toLocaleUpperCase());
+// console.log(text.search("ایپسوم"));
+// let text2 = text.replace("لورم", "امین");
+// console.log(text2);
+// let text3 = "       amin     23321321               "
+// console.log(text3.trim());
+// console.log(text.startsWith("لورم"));
+// console.log(text.endsWith("گیرد."));
+// let id = '389216';
+// console.log(id.padStart(12, '0'));
+// console.log(id.padEnd(12, '0'));
+// let access = true;
+// let age = 15;
+// access = (age > 18) ? true : false;
+// console.log(access);
 
-// console.log(fname1);
-// console.log(fname2);
+// let site = "https://sainnaco.ir/"
+// let msg = (site.includes("https://sa")) ? "Site is safe" : "Site is not safe";
+// console.log(msg);
+
+// let access = false;
+
+// let number = -12.68;
+// console.log(Boolean(number));
+
+// let msg = "";
+// console.log(Boolean(msg));
+
+// undefined + 10 = undefined
+// null + 10 = 10
+
+// let age = 18;
+
+
+/////// List /////
+
+// let students = ['Ali', 'Hassan', 'Zahra', 'Mahsa'];
+// console.log(students.length);
+// console.log(students[0]);
+// console.log(typeof students);
+// console.log(students.toString());
+// let str_students = students.toString();
+// console.log(str_students);
+// new_students = str_students.split(",")
+// console.log(typeof new_students);
 
 
 
-// console.log(fname);
-// // var fname = "Amin";
-// let fname = "Amin";
-
-// 2 * 2 = 4
-
-// const fname = "Amin";
-// console.log(fname);
-// fname = "Zahra";
-// console.log(fname);
-
-// Primitive Data Type
-
-// number
-// string 
-// boolean 
-// null 
-// undefined 
-// symbol 
 
 
 
-// non-Primitive Data Type
 
-// array
-// object
-// function
-// date
-// regix
