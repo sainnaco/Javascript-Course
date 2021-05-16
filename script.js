b8093fe7237a490826944b5028a07111e0b9cfb5
Amin Pourzare <apourzare@gmail.com>
2021-05-16 16:37:24 +0430

    Session 03 - Work with array list

--- script.js
+++ script.js
@@ -1,62 +1,68 @@
-// let fname = "Amin";
-// let lname = "Pourzare";
-// console.log(fname + " " + lname);
-// console.log(`${fname} ${lname}`);
+// let sum = function (a, b) {
+//     // c = a + b;
+//     // return c;
+//     // console.log(c);
+//     return a + b;
+// }
 
-let text = "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد."
-// console.log(text.length);
-// console.log(text[0]);
-// console.log(text.slice(0,4));
-// console.log(text.substr(5,40));
-// console.log(text.includes("لورم"));
+// let i = 20;
+// let j = 18;
+// let n1 = 30, n2 = 16;
+// console.log(sum(n1, n2));
+// // sum(17, 65);
 
-// let en_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet."
-// console.log(en_text.toLocaleLowerCase());
-// console.log(en_text.toLocaleUpperCase());
-// console.log(text.search("ایپسوم"));
-// let text2 = text.replace("لورم", "امین");
-// console.log(text2);
-// let text3 = "       amin     23321321               "
-// console.log(text3.trim());
-// console.log(text.startsWith("لورم"));
-// console.log(text.endsWith("گیرد."));
-// let id = '389216';
-// console.log(id.padStart(12, '0'));
-// console.log(id.padEnd(12, '0'));
-// let access = true;
-// let age = 15;
-// access = (age > 18) ? true : false;
-// console.log(access);
-
-// let site = "https://sainnaco.ir/"
-// let msg = (site.includes("https://sa")) ? "Site is safe" : "Site is not safe";
-// console.log(msg);
-
-// let access = false;
-
-// let number = -12.68;
-// console.log(Boolean(number));
-
-// let msg = "";
-// console.log(Boolean(msg));
-
-// undefined + 10 = undefined
-// null + 10 = 10
-
-// let age = 18;
+// let sum = (a, b) => { return a + b; }
+// let sum = (a, b) => {
+//     return a + b
+// }
+// let sum = (a, b) => (a + b);
+// console.log(sum(11, 76));
 
 
-/////// List /////
-
-// let students = ['Ali', 'Hassan', 'Zahra', 'Mahsa'];
-// console.log(students.length);
+let students = ['Amin', "Mehrsa", "Mohadesse", "Ali", "Zahra", "Milad"];
+// console.log(students);
+// students.push("Zahra");
+// console.log(students);
+// students.pop();
+// console.log(students);
+// students.shift();
+// console.log(students);
+// students.unshift("Zahra", "Milad");
+// console.log(students);
+// students.splice(1, 2);
+// console.log(students);
+// students.splice(3, 0, "Zahra", "Milad");
+// console.log(students);
+// students.splice(3, 1, "Zohre", "Nazi", "Amir");
+// console.log(students);
+// let teachers = students.slice(0, 3);
+// console.log(teachers);
+let numbers = [1, 16, 37, 4, 20, [17, 64, 93, 21]];
+// students.sort()
+// console.log(students);
+// numbers.sort()
+// numbers.sort((a,b) => a - b)
+// console.log(numbers);
+// students.sort().reverse();
+// console.log(students);
+// numbers.sort((a, b) => a - b).reverse()
+// console.log(numbers);
+// console.log(students.includes("Amin"));
+// console.log(students.includes("Hamid"));
+// let data = students.concat(numbers)
+// console.log(data);
+// console.log(students.indexOf("Hamid"));
+// console.log(numbers.find((n) => n < 20));
+// console.log(numbers.filter((n) => n < 20));
 // console.log(students[0]);
-// console.log(typeof students);
-// console.log(students.toString());
-// let str_students = students.toString();
-// console.log(str_students);
-// new_students = str_students.split(",")
-// console.log(typeof new_students);
+// console.log(numbers);
+// console.log(numbers[5]);
+// console.log(numbers[5][2]);
+
+
+
+
+
 
 
 
