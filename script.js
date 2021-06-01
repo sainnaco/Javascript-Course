9b640bd415f509305aee13b7440196f2025e2f0b
Amin Pourzare <apourzare@gmail.com>
2021-06-01 16:48:46 +0430

    Working with DOM

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
-// let n = 1000000013
-// let b = (n ** 0.5)
-// console.log(b);
-// let c = parseInt(b)
-// console.log(c + 1);
-// let d = ((c + 1) ** 0.5)
-// console.log(parseInt(d + 1));
-// let e = ((d + 1) ** 0.5)
-// console.log(parseInt(e+1));
+// let data = document.getElementsByTagName('h3')
+// let data = document.getElementById('article_00')
+// let data = document.getElementsByClassName('article_title')
+// let data = document.querySelector('body > div > article:nth-child(2) > h3')
+// console.log(typeof titles);
 
+// titles.forEach(title => {
+//     console.log(title);
+// });
 
-// let students = ['Amin', 31, 'Male', 87, 187]
-
-// console.log(students[2]);
-// let users = {
-//     'user1': {
-//         'name': 'Amin',
-//         'family': 'Pourzare'
-//     },
-//     'user2': {
-//         'name': 'Mehrsa',
-//         'family': 'Habibi'
-//     },
-//     token: 'fsd[pfojksdfmsfsfk'
+// for (let index = 0; index < titles.length; index++) {
+//     const element = titles[index];
+//     console.log(element.innerText);
 // }
 
-// console.log(users.token);
-// isPrime(1000)
+// console.log(article_00.innerText);
 
-// function isPrime(number) {
-//     // 
+// console.log(typeof data);
+
+// for (let index = 0; index < data.length; index++) {
+//     const element = data[index];
+//     console.log(element.innerText);
+    
 // }
 
-// // hasPrime(1000) Error
-// const hasPrime = function (number) {
-//     // 
-// }
-// hasPrime(12437)
+// console.log(typeof data);
+// console.log(data.innerText);
+// data.innertText = " Customize"
+// data.style.color = '#cccccc'
+// data.innerText += " Customize"
 
-// DOM
-// console.log(document.location);
-let titleElement = document.getElementById('title')
-titleElement.style.color = 'pink'
-console.log(titleElement);
+// for (let index = 0; index < data.length; index++) {
+//     const element = data[index];
+//     // element.innertText = 'AA'
+//     // console.log(element.innerText);
+//     // element.style.color = '#CCCCCC'
+//     // element.style.fontSize = '3.5rem';
+//     // element.innerText += " ----"
+//     // console.log(element.innerHTML);
+//     element.innerHTML += ' <span style="background: #ff0000; display: none;">New</span>'
+//     console.log(element.innerHTML);
+//     console.log("---------------");
+//     console.log(element.innerText);
+//     console.log("---------------");
+//     console.log(element.textContent);
+// }
+// let date = new Date()
+// let time = Date.now()
+// console.log(date.get);
+let num = 0;
+let hour = document.getElementById('hour')
+let minute = document.getElementById('minute')
+let second = document.getElementById('second')
+
+function showTime() {
+    let date = new Date()
+    hour.innerText = date.getHours()
+    minute.innerText = date.getMinutes()
+    second.innerText = date.getSeconds()
+}
+setInterval('showTime()', 1000);
